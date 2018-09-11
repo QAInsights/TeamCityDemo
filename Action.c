8e5614aee4a018361b22bb2eb39e52c59e0a7913
NaveenKumar <catch.nkn@gmail.com>
2018-09-10 20:41:07 -0500

    Draft

---
+++ Action.c
@@ -0,0 +1,19 @@
+Action()
+{
+lr_start_transaction("01_Launch");
+
+			web_custom_request("web_custom_request",
+		"URL=https://www.example.com",
+		"Method=GET",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Body=",
+		LAST);
+
+	lr_end_transaction("01_Launch", LR_AUTO);
+
+
+
+	return 0;
+}
